acdffa2fd36b66c14202e722b2acc8acd9c3e2c8
Mauricio Moccelin <mauriciomoccellin@hotmail.com>
2019-09-16 21:54:08 -0300

    Update biseccao.c

--- biseccao.c
+++ biseccao.c
@@ -2,21 +2,31 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <limits.h>
+#include <time.h>
 
 #include "omp.h"
 
 // gcc -fopenmp -g biseccao.c -o biseccao -lm
 // ./biseccao
 
-void obter_argumentos();
 void mostrar_argumento();
 void calcular();
 float funcao(float intervalo);
 void resposta_formatada();
+void mostrar_tempo_processamento(clock_t begin, clock_t end);
+
+void calcular_com_32threads();
+void calcular_com_16threads();
+void calcular_com_8threads();
+void calcular_com_4threads();
+void calcular_com_2threads();
+void calcular_com_1threads();
+void calcular_com_nthreads();
 
 float intervalo_inicial = 0;
-float intervalo_final = 0;
-float toleracia = 0;
+float intervalo_final = 2;
+float toleracia = 0.00000000000001;
 int numero_threads = 1;
 
 float raiz = 0;
@@ -25,33 +35,65 @@ int iteracoes = 0;
 
 int main ()
 {
-  obter_argumentos();
-  mostrar_argumento();
-  calcular();
-  resposta_formatada();
+  // calcular_com_32threads();
+  // calcular_com_16threads();
+  // calcular_com_8threads();
+  // calcular_com_4threads();
+  // calcular_com_2threads();
+  calcular_com_1threads();
   return 0;
 }
 
-void obter_argumentos() 
+void calcular_com_32threads()
 {
-  puts("ARGUMENTOS");
-  
-  puts("NUMERO_THREADS");
-  scanf("%d", &numero_threads);
-  
-  puts("INTERVALO_INICIAL");
-  scanf("%f", &intervalo_inicial);
-  
-  puts("INTERVALO_FINAL");
-  scanf("%f", &intervalo_final);
+  calcular_com_nthreads(32);
+}
 
-  puts("TOLERACIA");
-  scanf("%f", &toleracia);
+void calcular_com_16threads()
+{
+  calcular_com_nthreads(16);
+}
+
+void calcular_com_8threads()
+{
+  calcular_com_nthreads(8);
+}
+
+void calcular_com_4threads()
+{
+  calcular_com_nthreads(4);
+}
+
+void calcular_com_2threads()
+{
+  calcular_com_nthreads(2);
+}
+
+void calcular_com_1threads()
+{
+  calcular_com_nthreads(1);
+}
+
+void calcular_com_nthreads(int threads)
+{
+  numero_threads = threads;
+  clock_t begin = clock();
+  mostrar_argumento();
+  calcular();
+  resposta_formatada();
+  clock_t end = clock();
+  mostrar_tempo_processamento(begin, end);
+}
+
+void mostrar_tempo_processamento (clock_t begin, clock_t end) 
+{
+  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  printf("Número Threads: %d Tempo de processamento: %f\n", numero_threads, time_spent);
 }
 
 void mostrar_argumento()
 {
-  system("cls | clear");
+  puts("******************************************************");
   puts("ARGUMENTOS:\n");
   printf("NUMERO_THREADS: %i\t", numero_threads);
   printf("INTERVALO_INICIAL: %.4f\t", intervalo_inicial);
@@ -82,6 +124,7 @@ void calcular ()
       intervalo_inicial = pontoMedio;
     
     erro = fabs(intervalo_inicial - intervalo_final);
+    if (iteracoes == 1000) break;
   }
 
   raiz = pontoMedio;
@@ -89,7 +132,12 @@ void calcular ()
 
 float funcao(float intervalo) 
 {
-  return (pow(intervalo, 3) - 9 * intervalo + 5);
+  return (
+    pow(intervalo, 5) -
+    pow(intervalo, 4) -
+    pow(intervalo, 3) -
+    pow(intervalo, 2)
+  );
 }
 
 void resposta_formatada()
